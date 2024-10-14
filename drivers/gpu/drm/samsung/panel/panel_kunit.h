f84506d839e9a5ae18ff8410a356fda3e038d2c5
Joel Gómez <thegame455min@gmail.com>
2024-10-14 02:18:04 +0000

    drivers: samsung: panel: fix build without SEC KUNIT
    Signed-off-by: Joel Gómez <thegame455min@gmail.com>

--- drivers/gpu/drm/samsung/panel/panel_kunit.h
+++ drivers/gpu/drm/samsung/panel/panel_kunit.h
@@ -1,6 +1,6 @@
 #ifndef __PANEL_KUNIT_H__
 #define __PANEL_KUNIT_H__
-#include <kunit/mock.h>
+#include "kunit_mock_dummy.h"
 #ifndef KUNIT_EXPECT_TRUE_MSG
 #define KUNIT_EXPECT_TRUE_MSG(_test, _condition, _msg) KUNIT_EXPECT_TRUE(_test, _condition)
 #endif
