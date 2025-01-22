1b7ce657807b54acdc09eb441e252243fbdd7207
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-01-22 15:02:03 -0300

    Revert "FROMLIST: BACKPORT: mm: fix is_pinnable_page against on cma page"
    I picked this patch temporarily to try to solve the reserved memory issue, but it didn't help.
    
    This reverts commit d1b02ce639a9ec998066f1dd8c769c0a930ed1f8.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -495,12 +495,7 @@ unsigned long __get_pfnblock_flags_mask(struct page *page,
 	word_bitidx = bitidx / BITS_PER_LONG;
 	bitidx &= (BITS_PER_LONG-1);
 
-	/*
-	 * This races, without locks, with set_pfnblock_flags_mask(). Ensure
-	 * a consistent read of the memory array, so that results, even though
-	 * racy, are not corrupted.
-	 */
-	word = READ_ONCE(bitmap[word_bitidx]);
+	word = bitmap[word_bitidx];
 	return (word >> bitidx) & mask;
 }
 
